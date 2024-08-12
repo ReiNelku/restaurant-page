f4bd77e2aa91d68ac16090902c9e07bb39785def
Rei Nelku <85695388+ReiNelku@users.noreply.github.com>
2024-08-12 14:42:48 +0200

    feat:Create tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,38 @@
-import home from "./home.js";
+import displayHome from "./home.js";
+import displayMenu from "./menu.js";
 import "./style.css";
 
-home();
+(function () {
+  const content = document.querySelector("#content");
+
+  const homeTab = document.querySelector("#home");
+  homeTab.classList.add("active");
+  displayHome();
+  homeTab.addEventListener("click", createNavTabEventListeners);
+
+  const menuTab = document.querySelector("#menu");
+  menuTab.addEventListener("click", createNavTabEventListeners);
+})();
+
+function createNavTabEventListeners() {
+  content.textContent = "";
+  removeActiveClasses();
+
+  switch (this.textContent) {
+    case "Home":
+      this.classList.add("active");
+      displayHome();
+      break;
+
+    case "Menu":
+      this.classList.add("active");
+      displayMenu();
+      break;
+  }
+}
+
+function removeActiveClasses() {
+  document
+    .querySelectorAll("nav button")
+    .forEach((btn) => btn.classList.remove("active"));
+}
